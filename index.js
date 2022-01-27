92f1d55563ef82325888ac438ef158b7992be21a
Hassan Zahar Rifat <64719808+iFall-iFail@users.noreply.github.com>
2022-01-27 10:04:28 +0600

    Created User Route Requests

--- index.js
+++ index.js
@@ -26,7 +26,51 @@ const run = async() => {
         const postCollection = database.collection(process.env.DB_COLLECTION_BLOG);
 
         // Requests
-
+        app.post('/users', async (req, res) => {
+            const newUser = req.body;
+            const result = await userCollection.insertOne(newUser);
+            res.json(result);
+          });
+    
+          app.put('/users', async (req, res) => {
+            const newUser = req.body;
+            const filter = {email: newUser.email};
+            const option = {upsert: true};
+    
+            const upsertedDoc = {
+                $set: newUser
+            };
+            
+            const result = await userCollection.updateOne(filter, upsertedDoc, option);
+            res.json(result);
+          });
+    
+          app.put('/users/:email', async (req, res) => {
+            const { email } = req.params;
+            const filter = { email: email };
+            const replacedDoc = {
+                $set: { isAdmin: true }
+            };
+    
+            const result = await userCollection.updateOne(filter, replacedDoc);
+            res.json(result);
+          });
+    
+          app.get('/users/:email', async (req, res) => {
+            const { email } = req.params;
+            const query = { email: email };
+    
+            const matchedUser = await userCollection.findOne(query);
+            
+            if(matchedUser) {
+              res.json(matchedUser);
+            }
+    
+            else {
+              res.send({});
+            }
+    
+          });
 
 
     } finally {
